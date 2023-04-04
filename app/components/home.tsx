5193b7c6b11fdfe4c5a65728ee0e6b0c938384ac
shibushi233 <shishicscs994@gmail.com>
2023-04-04 16:39:59 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -115,9 +115,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>星造AI</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            体验属于你的AI助手
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
